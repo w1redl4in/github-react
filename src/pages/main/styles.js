5530a6e66d5799b73563c9d867c7e5d603d7a6bd
Felipe <felipe1180@hotmail.com>
2020-02-16 14:41:58 -0300

    fix border shadow

--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   max-width: 700px;
   background: #fff;
   border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
   padding: 30px;
   margin: 80px auto;
 
